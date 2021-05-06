fae931dfb11161997035ea730cc9b87fb0ddfafc
satooru <16670493+satooru@users.noreply.github.com>
2021-05-05 21:57:52 -0300

    refactor Menu Page

--- src/main/java/pages/Menu.java
+++ src/main/java/pages/Menu.java
@@ -2,20 +2,29 @@ package pages;
 
 import utils.WebDriverManager;
 
-import static utils.PropertiesReader.getPontoProperties;
-
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Menu {
 
-    private static WebDriverManager driver = WebDriverManager.getInstance();
+    WebDriver driver;
+    WebDriverWait wait;
 
-    // By - WebElements
-    static String xpathApontamentoMensal = "//div[@id='conteudo']//a[contains(@href, 'Lancamento')]";
+    @FindBy(xpath = "//div[@id='conteudo']//a[contains(@href, 'Lancamento')]")
+    private WebElement linkApontamentoMensal;
 
-    public static void abrirDigitacaoDeMarcacao() {
-        driver.waitElementToBeClickableByXPath(xpathApontamentoMensal, 3);
-        driver.clickElementByXPath(xpathApontamentoMensal);
-        driver.waitPageLoad();
+    public Menu() {
+        driver = WebDriverManager.getInstance().getWebDriver();
+        wait = WebDriverManager.getInstance().getWebDriverWait();
+        PageFactory.initElements( driver, this);
+    }
+
+    public void clickDigitacaoDeMarcacao() {
+        wait.until(ExpectedConditions.elementToBeClickable(linkApontamentoMensal));
+        linkApontamentoMensal.click();
     }
 }
