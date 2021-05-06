5ead33f0e43c5e7fef8e53b476c3d70542485ec8
satooru <16670493+satooru@users.noreply.github.com>
2021-05-05 21:01:48 -0300

    added RunnerTest

---
+++ src/test/java/runner/RunnerTest.java
@@ -0,0 +1,17 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = {"steps", "hooks"},
+        plugin = {"pretty"},
+        features = {"src/test/resources/"},
+        dryRun = false,
+        tags = {"@ponto"})
+public class RunnerTest {
+
+}
